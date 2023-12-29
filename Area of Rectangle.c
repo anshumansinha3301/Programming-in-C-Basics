b11dfcfe9c5b8a3fa7e94d5632d133e72ae5cc68
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-29 09:48:05 +0530

    Add files via upload
    C program to Find Area of Rectangle

---
+++ Area of Rectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a,b,area;
+    printf("enter two numbers :");
+    scanf("%d %d",&a ,&b);
+    
+    area = a*b;
+    printf("area:%d",area);
+
+
+    getch();
+    return 0;}
+
+    
+    
