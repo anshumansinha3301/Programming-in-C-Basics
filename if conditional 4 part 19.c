d93def0db4110c3ea028927990c250d075061d2c
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-30 20:09:31 +0530

    Add files via upload

---
+++ if conditional 4 part 19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    printf("enter an integer: ");
+    scanf("%d", &x);
+
+    if(x>=0) printf("%d", x);
+    if(x< 0) 
+    x = x*(-1); //formula for changing negative into positive
+    printf("the absolute value of x is %d", x);
+    return 0;
+}
