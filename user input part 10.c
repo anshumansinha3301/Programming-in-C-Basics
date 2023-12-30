8ef4b8cad41019a031b9454777964a49247430d0
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-30 20:04:46 +0530

    Add files via upload

---
+++ user input part 10.c
@@ -0,0 +1,11 @@
+// square of number
+#include<stdio.h>
+
+int main(){
+    int x;
+    printf("enter a number: ");
+    scanf("%d", &x);
+    printf("square of number is: %d", x*x);
+
+    return 0;
+}
