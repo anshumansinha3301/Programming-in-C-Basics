7cb68b15d24bca4a87a535d304eeef4f99000a7b
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-30 22:48:32 +0530

    Add files via upload

---
+++ Question 1.c
@@ -0,0 +1,15 @@
+/*Ramesh basic salary is input through the keyboard, 
+His dearness allowance is 40% of basic salary,
+and house rent allowance is 20% of basic salary. Write a program to calculate 
+his gross salary*/
+#include <stdio.h>
+
+int main() {
+    float basicSalary, grossSalary;
+    printf("Enter basic salary: ");
+    scanf("%f", &basicSalary);
+    grossSalary = basicSalary * 1.6; // Gross salary = basic salary + 40% + 20%
+    printf("Gross salary: %.2f\n", grossSalary);
+    return 0;
+}
+
