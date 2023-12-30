f61c99e1d0cfb3d61fbc0ab97e266dfe400c04bc
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-30 19:44:07 +0530

    Add files via upload

---
+++ operators part 6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{int x = 55;
+    printf("%d\n", x);
+
+    x++;
+    printf("after increment %d\n", x);
+
+    x--;
+    printf("after decrement %d\n", x);
+    return 0;}
