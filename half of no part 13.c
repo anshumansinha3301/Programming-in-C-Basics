b58db2ab5e2fb80a2da1f051169070b5c4cf5b0a
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-30 20:06:20 +0530

    Add files via upload

---
+++ half of no part 13.c
@@ -0,0 +1,13 @@
+// take integer x as input and print half of the number
+#include<stdio.h>
+
+int main(){
+    int x;
+    printf("enter a number: ");
+    scanf("%d", &x );
+    // typecasting process below
+    float f = (float)x;
+    printf("%f", f/2); // here x got typecasted from int to float
+    
+    return 0;
+}
