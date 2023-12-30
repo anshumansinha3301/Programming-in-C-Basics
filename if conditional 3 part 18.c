4e3747c33954a72cff176e2d6933fcd128ce2999
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-30 20:08:41 +0530

    Add files via upload

---
+++ if conditional 3 part 18.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    printf("enter an integer: ");
+    scanf("%d", &x);
+
+    if(x % 5 == 0) printf("the number is divisible by 5");
+    if(x % 5 != 0) printf("the number is not divisible by 5");
+    return 0;
+}
