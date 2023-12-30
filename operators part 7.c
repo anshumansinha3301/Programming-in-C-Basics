859063b3dce310d4e56511a72009c2f28ea1cc90
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2023-12-30 20:03:18 +0530

    Add files via upload

---
+++ operators part 7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int x = 5;
+    int y = 8;
+
+    printf("%d\n", x);
+    printf("%d\n", x++); // first work then add
+    printf("%d\n",x);
+
+    printf("%d\n", y);
+    printf("%d\n", ++y); // first add then work
+    printf("%d\n",y);
+    
+    return 0;
+
+}
