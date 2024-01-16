fba8ec128945ed6d74e5896c8fa020c3d9026762
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-16 08:58:28 +0530

    Compound Interest.c

--- Compound Interest.c
+++ Compound Interest.c
@@ -1,47 +1,12 @@
 // C program to calculate Compound Interest 
-#include <stdio.h>
-
-
-// For using pow function we must  
-// include math.h 
+#include <stdio.h>  
 #include<math.h>
-
-
-// Driver code 
-
-int
-main ()
+int main ()
 {
-
-  // Principal amount 
-
   double principal = 10000;
-
-
-
-  // Annual rate of interest 
-
   double rate = 5;
-
-
-
-  // Time 
-
   double time = 2;
-
-
-
-  // Calculating compound Interest 
-
-  double Amount = principal * ((pow ((1 + rate / 100),
-
-				     time)));
-
+  double Amount = principal * ((pow ((1 + rate / 100),time)));
   double CI = Amount - principal;
-
-
-
   printf ("Compound Interest is : %lf", CI);
-
-  return 0;
-}
+  return 0;}
