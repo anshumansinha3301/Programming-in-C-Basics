d496d386b32874b9b627179c7b5f7c7a42dba0ee
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-16 09:05:08 +0530

    sum of two numbers part 11.c

--- sum of two numbers part 11.c
+++ sum of two numbers part 11.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
-
 int main(){
     int x;
     printf("enter first number: ");
     scanf("%d", &x);
-
     int y;
     printf("enter second number: ");
     scanf("%d", &y);
-
     int z = (x + y);
     printf("the sum of two number is: %d", z);
-    return 0;
-}
+    return 0;}
