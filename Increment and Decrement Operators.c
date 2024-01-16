a1234e61fb09e2e1874e39d4abe4477d0283b095
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-16 09:04:02 +0530

    Increment and Decrement Operators.c

--- Increment and Decrement Operators.c
+++ Increment and Decrement Operators.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int main(){
     int num1 = 5, num2 = 10;
-
     printf("Original value of num1: %d\n", num1);
     printf("Original value of num2: %d\n", num2);
-
     // Increment operator (++) for num1
     num1++;
     printf("After incrementing num1, it is now: %d\n", num1);
-
     // Decrement operator (--) for num2
     num2--;
     printf("After decrementing num2, it is now: %d\n", num2);
