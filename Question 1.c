3c4660c249ad111baa2af02146397f767e6e153b
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-17 09:02:48 +0530

    Question 1.c

--- Question 1.c
+++ Question 1.c
@@ -2,6 +2,8 @@
 His dearness allowance is 40% of basic salary,
 and house rent allowance is 20% of basic salary. Write a program to calculate 
 his gross salary*/
+
+// the references of these question are from either Ansi C or let us C book //
 #include <stdio.h>
 
 int main() {
