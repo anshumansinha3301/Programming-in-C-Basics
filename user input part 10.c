6b2c67b171ae43879cb1b490ffdeaf87f5cd2e74
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-18 08:59:50 +0530

    user input part 10.c

--- user input part 10.c
+++ user input part 10.c
@@ -1,11 +1,8 @@
 // square of number
 #include<stdio.h>
-
 int main(){
     int x;
     printf("enter a number: ");
     scanf("%d", &x);
-    printf("square of number is: %d", x*x);
-
-    return 0;
-}
+    printf("square of number is: %d", x*x); // here we multiplied the number to itself to get the square
+    return 0;}
