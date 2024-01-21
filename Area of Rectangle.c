e37a75726115ae1bd9d15feea08edfff3940dc55
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-21 10:27:37 +0530

    Area of Rectangle.c

--- Area of Rectangle.c
+++ Area of Rectangle.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 int main(){
     int a,b,area;
     printf("enter two numbers :");
     scanf("%d %d",&a ,&b);
     area = a*b;
     printf("area:%d",area);
-    getch();
     return 0;}
 
     
