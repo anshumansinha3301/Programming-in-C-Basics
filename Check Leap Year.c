e5d77e0b541aa4be9820f086e5aa507b8ad530ec
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-22 18:45:24 +0530

    Check Leap Year.c

--- Check Leap Year.c
+++ Check Leap Year.c
@@ -3,7 +3,6 @@ int main() {
    int year;
    printf("Enter a year: ");
    scanf("%d", &year);
-
    // leap year if perfectly divisible by 400
    if (year % 400 == 0) {
       printf("%d is a leap year.", year);
@@ -22,6 +21,4 @@ int main() {
    else {
       printf("%d is not a leap year.", year);
    }
-
-   return 0;
-}
+   return 0;}
