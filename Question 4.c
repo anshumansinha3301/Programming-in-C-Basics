cd07cbe53016302cd3714548c48207cfa30948a8
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-24 08:09:12 +0530

    Question 4.c

--- Question 4.c
+++ Question 4.c
@@ -1,7 +1,6 @@
 /*Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a program 
 to convert this temperature into Centigrade degrees.*/
 #include <stdio.h>
-
 int main() {
     double fahrenheit, celsius;
 
