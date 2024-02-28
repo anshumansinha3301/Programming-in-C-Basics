fc8baaf67d1c65a1eb94d7bfb2635bc568185602
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-28 05:32:37 +0530

    LCM of Two Numbers.c

--- LCM of Two Numbers.c
+++ LCM of Two Numbers.c
@@ -3,7 +3,7 @@ int main() {
     int n1, n2, max;
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
-
+ 
     // maximum number between n1 and n2 is stored in max
     max = (n1 > n2) ? n1 : n2;
     while (1) {
