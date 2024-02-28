466d6f008acd63705fd40da743824bfbbe4ac86f
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-28 05:33:07 +0530

    behaviour operator 15.c

--- behaviour operator 15.c
+++ behaviour operator 15.c
@@ -6,7 +6,6 @@ int main(){
     printf("%f", x);
     return 0;
 }
-
 /* if we did float x = 5/2 and did printf("%f", x); the answer would be 2.000 
 because in float x = 5/2 [2 is considered an integer] so the answer does  not came 
 as float*/
