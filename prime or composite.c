83b6e7ee871510731a7ceb62ee160cb1ac52caae
rogercoding <161927447+rogercoding@users.noreply.github.com>
2024-03-05 15:07:05 +0530

    prime or composite.c

---
+++ prime or composite.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int isPrime(int, int);
+
+int main()
+{
+    int num, prime;
+    printf("Enter a positive number to check if Prime: ");
+    scanf("%d", &num);
+    prime = isPrime(num, num/2);
+    if(prime == 1)
+    {
+        printf("\n\n%d is a prime number\n\n", num);
+    }
+    else
+    {
+        printf("\n\n%d is a Composite number\n\n", num);
+    }
+    return 0;
+}
+
+
+int isPrime(int n, int i)
+{
+    if(i == 1)
+        return 1;   
+    else
+    {
+        if(n%i == 0)
+            return 0;
+        else
+            isPrime(n, i-1);   
+    }
+}
