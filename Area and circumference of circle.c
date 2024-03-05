8229246ac5cc31eefe1dff6d0c7fb01db78a43ec
rogercoding <161927447+rogercoding@users.noreply.github.com>
2024-03-05 15:18:27 +0530

    Area and circumference of circle.c

---
+++ Area and circumference of circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int rad;
+    float PI = 3.14, area, ci;
+    printf("\nEnter the radius of the circle: ");
+    scanf("%d", &rad);
+    area = PI*rad*rad;
+    printf("\n\n\n Area of the circle is: %f ", area);
+    ci = 2*PI*rad;
+    printf("\n\n\n Circumference of the circle is: %f", ci);
+    return 0;
+}
