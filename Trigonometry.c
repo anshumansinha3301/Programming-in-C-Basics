2810a944903ca9f089340f7f0da3fcc1f6754043
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-03-11 18:29:04 +0530

    Trigonometry.c

---
+++ Trigonometry.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double angle;
+
+    printf("Enter the angle in degrees: ");
+    scanf("%lf", &angle);
+
+    double angleInRadians = angle * (M_PI / 180.0);
+
+    printf("Sine of %.2lf degrees: %.4lf\n", angle, sin(angleInRadians));
+    printf("Cosine of %.2lf degrees: %.4lf\n", angle, cos(angleInRadians));
+
+    if (cos(angleInRadians) == 0) {
+        printf("Tangent is undefined for %.2lf degrees\n", angle);
+    } else {
+        printf("Tangent of %.2lf degrees: %.4lf\n", angle, tan(angleInRadians));
+    }
+
+    return 0;
+}
