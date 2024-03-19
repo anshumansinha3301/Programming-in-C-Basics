8157d14cece066e937ad776b4dc0a438aeea3de2
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-03-19 21:29:42 +0530

    Attack info game.c

--- Attack info game.c
+++ Attack info game.c
@@ -61,7 +61,7 @@ int main() {
             int choice;
             printf("Enter your choice: ");
             scanf("%d", &choice);
-     
+      
             switch (choice) {
                 case 1:
                     attackEnemy(&player, &enemies[i]);
