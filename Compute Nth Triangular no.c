50158e77df21349cdf4bfba1c3b9f86686bc4afc
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-17 09:44:14 +0530

    Add files via upload

---
+++ Compute Nth Triangular no.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int n, triangularNumber;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    triangularNumber = n * (n + 1) / 2;
+    printf("The %dth triangular number is: %d\n", n, triangularNumber);
+    return 0;
+}
