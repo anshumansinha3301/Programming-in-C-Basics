52c9f8ad0a1ffc600445cbd0d0477e9f870680ad
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-17 09:40:35 +0530

    Add files via upload

---
+++ Fibonacci sequence.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Sequence: ");
+    for (int i = 1; i <= n; ++i) {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    printf("\n");
+    return 0;
+}
