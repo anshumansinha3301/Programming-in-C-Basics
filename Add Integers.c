3275eb32a74fa246742eda713b9c303ce0bc4f50
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-10-11 23:44:00 +0530

    Update Add Integers.c

--- Add Integers.c
+++ Add Integers.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
-    // calculate the sum
+    // calculate the sum  
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
